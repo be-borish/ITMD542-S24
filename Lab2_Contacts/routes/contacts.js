c5c64cfd9338ec20bc367afb4a9429cbf231a2ad
Bori Kim <bkim35@hawk.iit.edu>
2024-02-18 17:33:38 -0600

    add express validator

--- Lab2_Contacts/routes/contacts.js
+++ Lab2_Contacts/routes/contacts.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
 const contactsRepo = require('../src/contactsFileRepository');
 
 /* GET contacts listing. */
